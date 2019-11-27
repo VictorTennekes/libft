efab15ad5c3fd431f8de702fa5e6948344813880
Victor jan piet joostszoon Tennekes <vtenneke@f0r1s8.codam.nl>
2019-11-27 13:01:59 +0100

    compressed into one function

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,61 +6,38 @@
 /*   By: vtenneke <vtenneke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/01 09:46:05 by vtenneke       #+#    #+#                */
-/*   Updated: 2019/11/08 14:42:12 by vtenneke      ########   odam.nl         */
+/*   Updated: 2019/11/07 13:22:26 by vtenneke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 #include <stdlib.h>
 
-static size_t	ft_dubstrlen(const char *str1, const char *str2)
+char    *ft_strjoin(char const *s1, char const *s2)
 {
-	int len1;
-	int len2;
+    char    *dest;
+    size_t    i;
+    size_t    k;
 
-	len1 = 0;
-	len2 = 0;
-	while (str1[len1] != '\0')
-		len1++;
-	while (str2[len2] != '\0')
-		len2++;
-	return (len1 + len2);
-}
-
-static char		*ft_dubcpy(char *dest, char const *s1, char const *s2, size_t k)
-{
-	size_t i;
-
-	i = 0;
-	while (s1[k])
-	{
-		dest[k] = s1[k];
-		k++;
-	}
-	while (s2[i])
-	{
-		dest[k] = s2[i];
-		k++;
-		i++;
-	}
-	dest[k] = '\0';
-	return (dest);
-}
-
-char			*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*dest;
-	size_t	i;
-	size_t	k;
-
-	if (s1 == 0)
-		return (NULL);
-	k = 0;
-	i = ft_dubstrlen(s1, s2);
-	dest = (char*)malloc(i + 1);
-	if (!(dest))
-		return (NULL);
-	i = 0;
-	ft_dubcpy(dest, s1, s2, k);
-	return (dest);
+    k = 0;
+    if (s1 == 0)
+        return (NULL);
+    i = ft_strlen(s1) + ft_strlen(s2);
+    dest = (char*)malloc(i + 1);
+    if (!(dest))
+        return (NULL);
+    while (*s1)
+    {
+        dest[k] = *s1;
+        s1++;
+        k++;
+    }
+    while (*s2)
+    {
+        dest[k] = *s2;
+        s2++;
+        k++;
+    }
+    dest[k] = '\0';
+    return (dest);
 }
